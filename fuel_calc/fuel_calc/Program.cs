7166cabd09d060660bcf2194e0efb98b3f47a43c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-09-26 15:51:19 +0700

    Commit 1
    Create fuel calculator

---
+++ fuel_calc/fuel_calc/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Простая программа расчета среднего расхода топлива");
+Console.Write("Введите расстояние (км): ");
+
+double dist = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Потрачено топлива (л): ");
+
+double vol = Convert.ToDouble(Console.ReadLine());
+
+double cons = (vol * 100) / dist;
+
+string output = string.Format("Автомобиль прошел {0:F} км, потратил {1:F} литров топлива. Средний расход - {2:F} л / 100 км.", dist, vol, cons);
+
+Console.WriteLine(output);
+
+
