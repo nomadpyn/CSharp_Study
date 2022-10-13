63d631b387ce513e292ee6dce238ea60423d75a6
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-10-13 10:50:31 +0700

    Commit 11
    Create Strings HomeWork

---
+++ strings_hw/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string? data = Console.ReadLine();
+
+string[] strArr = data.Split();
+
+for(int i = 0; i < strArr.Length; i++)
+{
+    string temp = strArr[i];
+    temp = char.ToUpper(temp[0]) + temp.Substring(1);
+    strArr[i] = temp;
+}
+
+string data_up = String.Join(" ",strArr);
+
+Console.WriteLine(data_up);
