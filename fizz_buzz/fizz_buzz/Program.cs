d2ceea950beb9a47df246738277174568c626318
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-11-04 00:10:48 +0700

    Commit 18
    Add "fizz_buzz" programm

---
+++ fizz_buzz/fizz_buzz/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число от 1 до 100");
+int digit = Int32.Parse(Console.ReadLine());
+
+if(digit<1 || digit > 100)
+{
+    Console.WriteLine("Вы ввели неправильное число");
+}
+else
+{
+    if (digit % 3 != 0 && digit%5 !=0)
+    {
+        Console.WriteLine(digit);
+    }
+    else
+    {
+        if(digit % 3 == 0 && digit % 5 == 0)
+        {
+            Console.WriteLine("Fizz Buzz");
+        }
+        else
+        {
+            if (digit % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine("Buzz");
+            }
+        }
+    }
+}
