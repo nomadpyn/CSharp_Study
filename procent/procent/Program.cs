ab33a31cc3c174b25e4a0d60927c7a9b56be3d2e
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-11-04 00:01:23 +0700

    Commit 17
    Add "procent" programm

---
+++ procent/procent/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int digit = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите процент");
+int percent = Int32.Parse(Console.ReadLine());
+
+int result = digit * percent/100;
+
+Console.WriteLine(result);
