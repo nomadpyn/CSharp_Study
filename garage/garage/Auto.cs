c45e6e13246d28a074da6e365365895da421cd96
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-07 14:07:16 +0700

    Commit 20
    Create Garage HomeWork:
    - create class Auto

---
+++ garage/garage/Auto.cs
@@ -0,0 +1,17 @@
+﻿internal class Auto
+    {
+    protected string? name;
+    protected string? color;
+    protected uint max_speed;
+    public Auto(string nameP, string colorP, uint max_speedP)
+    {
+        this.name = nameP;
+        this.color = colorP;
+        this.max_speed = max_speedP;
+    }
+    public override string ToString()
+    {
+        return $"Авто {this.name} цветом {this.color} с макс скоростью {this.max_speed} км/ч";
+    }
+}
+
