4b6ab34af59ef0be3bd8c14211ac3c68ad99694c
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-11-10 12:35:05 +0700

    Commit 24
    Add byte_exception homework

---
+++ byte_exception/byte_exception/Program.cs
@@ -0,0 +1,78 @@
+﻿byte a = 0;
+byte b = 0;
+
+try
+{
+    Console.WriteLine("Введите число А");
+    a = checked(byte.Parse(Console.ReadLine()));
+    Console.WriteLine(a);
+    Console.WriteLine("Введите число B");
+    b = checked(byte.Parse(Console.ReadLine()));
+    Console.WriteLine(b);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message) ;
+}
+
+Sum(a, b);
+Diff(a, b);
+Mult(a, b);
+Divide(a, b);
+
+ void Sum(byte a, byte b)
+{
+    
+    try
+    {
+        Console.WriteLine($"Сумма двух чисел {checked((byte)(a + b))}");
+    }
+    catch(OverflowException e)
+    {
+        Console.WriteLine("Ошибка при сумме двух чисел");
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Diff(byte a, byte b)
+{
+    try
+    {
+        Console.WriteLine($"Разность двух чисел {checked((byte)(a - b))}");
+    }
+    catch (OverflowException e)
+    {
+        Console.WriteLine("Ошибка при разности двух чисел");
+        Console.WriteLine(e.Message);
+    }
+}
+
+void Mult(byte a, byte b)
+{
+    try
+    {
+        Console.WriteLine($"Произведение двух чисел {checked((byte)(a * b))}");
+    }
+    catch (OverflowException e)
+    {
+        Console.WriteLine("Ошибка при умножении двух чисел");
+        Console.WriteLine(e.Message);
+    }
+}
+void Divide(byte a, byte b)
+{
+    try
+    {
+        Console.WriteLine($"Делени двух чисел {checked((byte)(a / b))}");
+    }
+    catch (OverflowException e)
+    {
+        Console.WriteLine("Ошибка при делении двух чисел");
+        Console.WriteLine(e.Message);
+    }
+    catch(DivideByZeroException ez)
+    {
+        Console.WriteLine("Ошибка при делении на ноль");
+        Console.WriteLine(ez.Message);
+    }
+}
