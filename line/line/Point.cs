b2e832466cf9e9e222fff33732389d736209cf5a
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-14 12:48:37 +0700

    Commit 27
    Add line homework
    Create class Point

---
+++ line/line/Point.cs
@@ -0,0 +1,9 @@
+﻿    internal class Point
+    {
+    public int X { get; set; }
+    public int Y { get; set; }
+    public override string ToString()
+    {
+        return $"Точка X = {this.X}, Y = {this.Y}";
+    }
+}
