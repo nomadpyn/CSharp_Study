eacfe9a8312e4436c98be16411d08a90a0338689
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-14 12:57:45 +0700

    Commit 29
    line homework:
    add class Line

---
+++ line/line/Line.cs
@@ -0,0 +1,21 @@
+﻿    internal class Line
+    {
+    Point Start;
+    Point End;
+    
+        public Line(Point a, Point b)
+    {
+        this.Start = a;
+        this.End = b;
+    }
+    public Line(int ax,int ay,int bx,int by)
+    {
+        this.Start = new Point(ax, ay);
+        this.End = new Point(bx, by);
+    }
+    public override string ToString()
+    {
+       
+        return $"Прямая линия из точки {this.Start} в точку {this.End}";
+    }
+}
