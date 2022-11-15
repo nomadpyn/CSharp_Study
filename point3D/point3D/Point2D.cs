3e8461cef51451ae279f539de08234a94801156b
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-15 10:36:13 +0700

    Commit 34
    point3D homework:
    add class Point2D

---
+++ point3D/point3D/Point2D.cs
@@ -0,0 +1,15 @@
+﻿internal class Point2D <T> where T : struct
+{ 
+    public T X { get; set; }
+    public T Y { get; set; }
+    public Point2D(T x, T y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+    public Point2D()
+    {
+        X = default(T);
+        Y = default(T);
+    }
+}
