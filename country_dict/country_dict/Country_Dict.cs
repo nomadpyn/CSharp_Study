dac5c748990eb7bea7ad7e4dd4a50a355ec06303
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-17 12:48:24 +0700

    Commit 38
    country_dict homework:
    - add showEngRus method
    - add showRusEng method

--- country_dict/country_dict/Country_Dict.cs
+++ country_dict/country_dict/Country_Dict.cs
@@ -1,4 +1,6 @@
-﻿internal class Country_Dict
+﻿using System.Diagnostics;
+
+internal class Country_Dict
 {
     Dictionary<string, string> dict;
 
@@ -17,5 +19,21 @@
             ["Russia"] = "Россия",
             ["Mexico"] = "Мексика"
         };
+    }
+    public void showEngRus()
+    {
+        Console.WriteLine("In dictionary {0} words", this.dict.Count);
+        foreach(KeyValuePair<string,string> i in this.dict)
+        {
+            Console.WriteLine($"Eng: {i.Key}\tRus: {i.Value}");
+        }
+    }
+    public void showRusEng()
+    {
+        Console.WriteLine("В словаре {0} слов", this.dict.Count);
+        foreach (KeyValuePair<string, string> i in this.dict)
+        {
+            Console.WriteLine($"Rus: {i.Value}\tEng: {i.Key}");
+        }
     }
 }
