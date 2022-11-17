0dc22b29dbb936be30aacafd66d6aa52fca2499d
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-11-17 12:35:49 +0700

    Commit 37
    country_dict homework:
    create constructor bt default

--- country_dict/country_dict/Country_Dict.cs
+++ country_dict/country_dict/Country_Dict.cs
@@ -1,4 +1,21 @@
 ﻿internal class Country_Dict
 {
+    Dictionary<string, string> dict;
 
+    public Country_Dict()
+    {
+        dict = new Dictionary<string, string>
+        {
+            ["China"] = "Китай",
+            ["India"] = "Индия",
+            ["USA"] = "США",
+            ["Indonesia"] = "Индонезия",
+            ["Pakistan"] = "Пакистан",
+            ["Brazil"] = "Бразилия",
+            ["Nigeria"] = "Нигерия",
+            ["Bangladesh"] = "Бангладеш",
+            ["Russia"] = "Россия",
+            ["Mexico"] = "Мексика"
+        };
+    }
 }
